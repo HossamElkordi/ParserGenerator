d0f36dcc9777446ffdd74dbab5afdb1b1d684d7a
Hossam Elkordi <hossam_elkordi@yahoo.com>
2021-06-10 13:49:39 +0200

    Final Commit

--- TableGenerator.h
+++ TableGenerator.h
@@ -6,6 +6,7 @@
 #define PARSERGENERATOR_TABLEGENERATOR_H
 
 #include "FollowSetGenerator.h"
+#include <fstream>
 using namespace std;
 
 vector<vector<vector<string>>> GenerateTable(const map<string, vector<pair<string, vector<string>>>>& First,  FollowSetGenerator Follow,vector<string>Terminals,vector<string>NonTerminals);
